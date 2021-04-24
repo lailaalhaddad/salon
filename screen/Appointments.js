744ae2077d93e8a6815958a84e15a2fb8a57af12
lailaalhaddad <lalhaddad95@gmail.com>
2021-04-25 00:17:21 +0300

    Update Appointments.js

--- screen/Appointments.js
+++ screen/Appointments.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 import AppointmentList from "../components/Appointments/AppointmentList";
-import DropDownPicker from "react-native-dropdown-picker";
-import CreateAppointment from "../components/Appointments/CreateAppointment";
+// import DropDownPicker from "react-native-dropdown-picker";
+// import CreateAppointment from "../components/Appointments/CreateAppointment";
 import { Box, InnBox } from "../styles";
 // import { TableContent, CustomTable } from "../styles";
 
 const Appointments = () => {
   return (
     <View>
-      <View>
-        <DropDownPicker />
-      </View>
+      <View>{/* <DropDownPicker /> */}</View>
       <Box>
         <InnBox>
           <AppointmentList />
