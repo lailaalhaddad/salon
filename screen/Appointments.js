ecccfa9b7640f39bb697cf212c8234c4fec66709
lailaalhaddad <lalhaddad95@gmail.com>
2021-04-25 00:16:22 +0300

    i added the accordion, but i did not style it

--- screen/Appointments.js
+++ screen/Appointments.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import AppointmentList from "../components/Appointments/AppointmentList";
+import DropDownPicker from "react-native-dropdown-picker";
 import CreateAppointment from "../components/Appointments/CreateAppointment";
 import { Box, InnBox } from "../styles";
 // import { TableContent, CustomTable } from "../styles";
 
 const Appointments = () => {
   return (
-    <Box>
-      <InnBox>
-        <AppointmentList />
-      </InnBox>
-    </Box>
+    <View>
+      <View>
+        <DropDownPicker />
+      </View>
+      <Box>
+        <InnBox>
+          <AppointmentList />
+        </InnBox>
+      </Box>
+    </View>
   );
 };
 
